65319d5a893497271a285abeac7eb017d7a60916
gridbrown <59910838+gridbrown@users.noreply.github.com>
2020-10-02 21:03:58 +0900

    1002

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
 (() => {
+
+    //bx_slider 설정
     $(document).ready(function () {
         $('.fun_slide').bxSlider();
     });
+
+    //main_footer notice 탭 기능
+    const noticeTit = document.querySelectorAll(".notice_tit");
+    const detailList = document.querySelectorAll(".detail_list");
+
+
+    for (let i = 0; i < noticeTit.length; i++) {
+        noticeTit[i].addEventListener("click", function (e) {
+            e.preventDefault();
+            console.log("click");
+
+            for (let j = 0; j < noticeTit.length; j++) {
+                noticeTit[j].classList.remove("active");
+            }
+            e.target.classList.add("active");
+
+
+            if (noticeTit[i].classList.contains("active")) {
+                for (let k = 0; k < noticeTit.length; k++) {
+                    detailList[k].classList.remove("active")
+                }
+                detailList[i].classList.add("active")
+            }
+        })
+
+
+    }
+
+
+
+
+
 })();
