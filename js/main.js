a10a35699e0186d0e8c796071871ead301bb6f62
yujin mac <kimyu28jin@gmail.com>
2021-01-08 13:25:10 +0900

    영상 추가

--- js/main.js
+++ js/main.js
@@ -1,50 +1,41 @@
 (() => {
+  //bx_slider 설정
+  $(document).ready(function () {
+    $(".fun_slide").bxSlider();
+  });
 
-    //bx_slider 설정
-    $(document).ready(function () {
-        $('.fun_slide').bxSlider();
+  //main_footer notice 탭 기능
+  const noticeTit = document.querySelectorAll(".notice_tit");
+  const detailList = document.querySelectorAll(".detail_list");
+
+  for (let i = 0; i < noticeTit.length; i++) {
+    noticeTit[i].addEventListener("click", function (e) {
+      e.preventDefault();
+      console.log("click");
+
+      for (let j = 0; j < noticeTit.length; j++) {
+        noticeTit[j].classList.remove("active");
+      }
+      e.target.classList.add("active");
+
+      if (noticeTit[i].classList.contains("active")) {
+        for (let k = 0; k < noticeTit.length; k++) {
+          detailList[k].classList.remove("active");
+        }
+        detailList[i].classList.add("active");
+      }
     });
+  }
 
-    //main_footer notice 탭 기능
-    const noticeTit = document.querySelectorAll(".notice_tit");
-    const detailList = document.querySelectorAll(".detail_list");
+  const sildeF = document.querySelector(".slide1 img");
+  const sildeS = document.querySelector(".slide2 img");
+  window.addEventListener("resize", function () {
+    const container = document.querySelectorAll(".container");
 
-
-    for (let i = 0; i < noticeTit.length; i++) {
-
-        noticeTit[i].addEventListener("click", function (e) {
-            e.preventDefault();
-            console.log("click");
-
-            for (let j = 0; j < noticeTit.length; j++) {
-                noticeTit[j].classList.remove("active");
-            }
-            e.target.classList.add("active");
-
-
-            if (noticeTit[i].classList.contains("active")) {
-                for (let k = 0; k < noticeTit.length; k++) {
-                    detailList[k].classList.remove("active")
-                }
-                detailList[i].classList.add("active")
-            }
-        })
-
-
+    if (container[0].clientWidth == 980) {
     }
 
-
-
-    const sildeF = document.querySelector(".slide1 img");
-    const sildeS = document.querySelector(".slide2 img");
-    window.addEventListener("resize", function () {
-        const container = document.querySelectorAll(".container");
-        console.log(container[0].clientWidth)
-        if (container[0].clientWidth == 980) {
-
-        }
-
-        /*  window창이 500일때 이미지가 바뀜
+    /*  window창이 500일때 이미지가 바뀜
         if (container[0].clientWidth == 980) {
             sildeF.setAttribute('src', 'images/slide_mini_01.jpg')
             sildeS.setAttribute('src', 'images/slide_mini_02.png')
@@ -55,10 +46,5 @@
             sildeS.setAttribute('src', 'images/slide_img_02.png')
         }
 */
-    })
-
-
-
-
-
+  });
 })();
